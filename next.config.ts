8dc6aa58c3981e1a2b2a2e5be24a5e64888cbd72
Nikita <nikitashapovalov2007@gmail.com>
2026-03-26 20:38:08 +0300

    Remove upgrade-insecure-requests from CSP

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,6 @@ const contentSecurityPolicy = [
   "base-uri 'self'",
   "form-action 'self'",
   "frame-ancestors 'none'",
-  ...(isProduction ? ["upgrade-insecure-requests"] : []),
 ].join('; ')
 
 const nextConfig: NextConfig = {
